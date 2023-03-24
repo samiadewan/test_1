49320860c0466896341b8c3ff50be1e573454b9b
samiadewan <samia.dewan@brainstation-23.com>
2023-03-24 10:02:45 +0600

    testing

---
+++ GetSetmethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+namespace GetSetmethod
+{
+    class Person
+    {
+       public string Name
+        { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ob = new Person();
+            ob.Name="samia";
+            Console.WriteLine(ob.Name);
+        }
+    }
+}
